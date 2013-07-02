b6dcb143b0b7b3feeadd11557fdbfc38dda87697
Miguel Gonzalez <migonzalvar@gmail.com>
2013-07-02 11:00:31 +0200

    Remove config.py.in and use a static config.py

--- extensions/cpsection/modemconfiguration/config.py
+++ extensions/cpsection/modemconfiguration/config.py
@@ -1,6 +1,6 @@
-PROVIDERS_PATH = "@prefix@/usr/share/mobile-broadband-provider-info/serviceproviders.xml"
+PROVIDERS_PATH = "/usr/share/mobile-broadband-provider-info/serviceproviders.xml"
 PROVIDERS_FORMAT_SUPPORTED = "2.0"
-COUNTRY_CODES_PATH = "@prefix@usr/share/zoneinfo/iso3166.tab"
+COUNTRY_CODES_PATH = "/usr/share/zoneinfo/iso3166.tab"
 
 GCONF_SP_COUNTRY = '/desktop/sugar/network/gsm/country'
 GCONF_SP_PROVIDER = '/desktop/sugar/network/gsm/provider'
